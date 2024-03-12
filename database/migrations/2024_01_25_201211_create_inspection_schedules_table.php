60b5ed015e9d46a7affeb012ffa26cd40ba797c6
Mike Maltby <mike@ingersollrentall.ca>
2024-03-11 22:11:09 -0400

    make product_id nullable on inspection schedule migration

--- database/migrations/2024_01_25_201211_create_inspection_schedules_table.php
+++ database/migrations/2024_01_25_201211_create_inspection_schedules_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('product_inspection_schedules', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')
+                ->nullable()
                 ->constrained(table: 'products')
                 ->cascadeOnUpdate()
                 ->restrictOnDelete();
